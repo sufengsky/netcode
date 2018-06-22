b98c7d38e7ed48b8a544a67665b8579957f4dbfb
sufengsky <365490686@qq.com>
2018-06-22 15:12:53 +0800

    初始

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable=new Hashtable();
+            hashtable.Add("aa","abc");
+
+            foreach (var key in hashtable.Keys)
+            {
+                 Console.WriteLine(hashtable[key]);
+            }
+           
+        }
+    }
+}
