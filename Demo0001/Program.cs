6802f2bb36499b21120aa91cc62bad1328d45e08
sufengsky <365490686@qq.com>
2019-01-14 18:08:18 +0800

    增加示例

---
+++ Demo0001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo0001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
