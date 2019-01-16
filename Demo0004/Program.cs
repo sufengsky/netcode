7313d0cbd99783b72e5d6eacaabb3c2267646a27
sufengsky <365490686@qq.com>
2019-01-16 10:46:30 +0800

    linq基本使用

---
+++ Demo0004/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demo0004
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            var list = new List<string>
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            // var result = list.Where (x => x == "a").ToList ();
+            // foreach (var item in result)
+            // {
+            //     System.Console.WriteLine (item);
+            // }
+            var result = list.Select (x => x == "a").ToList();
+            foreach (var item in result)
+            {
+                System.Console.WriteLine (item);
+            }
+        }
+    }
+}
