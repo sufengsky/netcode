f94316580229b02d19cca39145e5cf8d81fec5ab
sufengsky <365490686@qq.com>
2019-01-23 20:29:56 +0800

    静态成员变量初始化的示例

---
+++ Demo0005/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo0005
+{
+    
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            System.Console.WriteLine(Test.GetCount());
+        }
+    }
+
+    public class Test
+    {
+        private static Dictionary<string, string> dic = new Dictionary<string, string> ();
+        static Test ()
+        {
+             dic.Add("a","a");
+        }
+
+        public static int GetCount ()
+        {
+            return dic.Count;
+        }
+    }
+}
