6e50d83d51e7807bc4e499c6b48c2b644618b357
sufengsky <365490686@qq.com>
2019-01-29 17:26:53 +0800

    多线程编程里的Parallel类的使用

---
+++ Demo0007/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo0007
+{
+    // 用于多线程编程的Parallel类的使用示例
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParallelLoopResult result=Parallel.For(
+                0,10,i=>{
+                    Console.WriteLine("{0},task:{1},thread:{2}",i,
+                    Task.CurrentId,Thread.CurrentThread.ManagedThreadId);
+                }
+            );
+
+            System.Console.WriteLine("Is completed:{0}",result.IsCompleted);
+        }
+    }
+}
