6d9f3e2e5bc379f8a41880997a8eb7977f0df21e
sufengsky <365490686@qq.com>
2019-01-29 17:36:03 +0800

    简单修改下程序

--- Demo0007/Program.cs
+++ Demo0007/Program.cs
@@ -7,16 +7,18 @@ namespace Demo0007
     // 用于多线程编程的Parallel类的使用示例
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            ParallelLoopResult result=Parallel.For(
-                0,10,i=>{
-                    Console.WriteLine("{0},task:{1},thread:{2}",i,
-                    Task.CurrentId,Thread.CurrentThread.ManagedThreadId);
+            ParallelLoopResult result = Parallel.For (
+                0, 10, i =>
+                {
+                    Console.WriteLine ("{0},task:{1},thread:{2}", i,
+                        Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep (10);
                 }
             );
 
-            System.Console.WriteLine("Is completed:{0}",result.IsCompleted);
+            System.Console.WriteLine ("Is completed:{0}", result.IsCompleted);
         }
     }
 }
