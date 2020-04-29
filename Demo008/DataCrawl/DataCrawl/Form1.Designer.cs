7f35bd53b9ec1b98976168da7657cc8eceb7614d
sufengsky <365490686@qq.com>
2020-04-30 00:05:38 +0800

    程序调试完善

--- Demo008/DataCrawl/DataCrawl/Form1.Designer.cs
+++ Demo008/DataCrawl/DataCrawl/Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.btnGetImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGetImage
             // 
-            this.btnGetImage.Location = new System.Drawing.Point(208, 231);
+            this.btnGetImage.Location = new System.Drawing.Point(139, 269);
             this.btnGetImage.Name = "btnGetImage";
             this.btnGetImage.Size = new System.Drawing.Size(75, 23);
             this.btnGetImage.TabIndex = 0;
@@ -41,14 +43,25 @@
             this.btnGetImage.UseVisualStyleBackColor = true;
             this.btnGetImage.Click += new System.EventHandler(this.btnGetImage_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(244, 269);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 37);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(746, 436);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnGetImage);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnGetImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Demo008/DataCrawl/DataCrawl/Form1.cs
+++ Demo008/DataCrawl/DataCrawl/Form1.cs
@@ -25,15 +25,15 @@ namespace DataCrawl
 
         private void btnGetImage_Click(object sender, EventArgs e)
         {
-            string url = "http://220.160.52.164:9085/super/pages/login/image.jsp?_time=" + DateTime.Now.ToString();
-            doGetImg(url);
+            doGetImg();
         }
-
-        private void doGetImg(string Url)
+        private void GetCookie()
         {
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url.ToString());
+                //要post提交的地址。先用HttpWebRequest进行请求以得到cookie，并保存起来在后面获取验证码的时候使用
+                string Url="http://220.160.52.164:9085/super/Framework_checkLogin.jspx";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
                 request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
                 request.Headers.Add(HttpRequestHeader.AcceptLanguage, "zh-CN");
                 request.Accept = "text/html, application/xhtml+xml,*/*";
@@ -46,6 +46,35 @@ namespace DataCrawl
                 request.Credentials = CredentialCache.DefaultCredentials;
                 request.Host = "220.160.52.164:9085";
                 request.CookieContainer = new CookieContainer(); //暂存到新实例
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                var st = request.CookieContainer.GetCookieHeader(request.RequestUri);
+                //将当前cookie存储到CookieContainer中
+                cookies.Add(response.Cookies);
+                response.Close();
+            }
+            catch { 
+            
+            }
+        }
+        private void doGetImg()
+        {
+            string url = "http://220.160.52.164:9085/super/pages/login/image.jsp?_time=" + DateTime.Now.ToString();
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "zh-CN");
+                request.Accept = "text/html, application/xhtml+xml,*/*";
+                request.KeepAlive = true;
+
+                request.UserAgent = "User-Agent Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.AllowAutoRedirect = true;
+                request.Credentials = CredentialCache.DefaultCredentials;
+                request.Host = "220.160.52.164:9085";
+                request.CookieContainer = cookies; //暂存到新实例
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
                 Stream responseStream = null;
@@ -69,17 +98,33 @@ namespace DataCrawl
                 }
                 cookies = request.CookieContainer; //保存cookies
                 response.Close();
-                //var cookiesstr = request.CookieContainer.GetCookieHeader(request.RequestUri); //把cookies转换成字符串
+                var cookiesstr = request.CookieContainer.GetCookieHeader(request.RequestUri); //把cookies转换成字符串
                 var img = Bitmap.FromStream(ms);
-                if (File.Exists("code.jpeg"))
-                {
-                    File.Delete("code.jpeg");
-                }
-                img.Save("code.jpg", ImageFormat.Jpeg);
+
+                this.pictureBox1.Image = img;
             }
-            catch
+            catch(Exception ex)
             {
+
             }
+        } 
+
+        private void doLogin()
+        {
+            string pwd = "5937ffad5ecd5f6dd9a83b83895e480f";//xc123456 md5加密后的字符串
+            string loginUrl = "http://220.160.52.164:9085/super/super/Framework_doLogin.jspx";
+        }
+
+        private void GetASingleInfo()
+        {
+            string url = "http://220.160.52.164:9085/fw/FwAction_toInfo.jspx?fw.id=BFC58877-B7CB-009A-E043-0A822906009A&mode=4&page=1&city1=350700&county1=350782&street1=350782003000&community1=350782003007&zgqk1=&pcjl1=&ydxz1=&lrfs1=&sfpkh1=undefined&sfygatb1=&jclx1=&sjqk1=&ywsgct1=&jglx1=&syyt1=&gzqk1=&gznr1=&fwlb1=&yy1=&timeq1=&timez1=&dqzt1=undefined&keywords1=";
+            string srl = "http://220.160.52.164:9085/fw/FwAction_toInfo.jspx?fw.id=BFC3D75C-F13A-01DA-E043-0A82290601DA&mode=4&page=1&city1=350700&county1=350782&street1=350782003000&community1=350782003007&zgqk1=&pcjl1=&ydxz1=&lrfs1=&sfpkh1=undefined&sfygatb1=&jclx1=&sjqk1=&ywsgct1=&jglx1=&syyt1=&gzqk1=&gznr1=&fwlb1=&yy1=&timeq1=&timez1=&dqzt1=undefined&keywords1=";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetCookie();
+            doGetImg();
         }
     }
 }
