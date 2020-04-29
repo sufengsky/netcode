e3329e887ad7f8ac535f4ad30b2f8060118a8f35
sufeng <365490686@qq.com>
2020-04-29 18:13:14 +0800

    项目初始化

---
+++ Demo008/DataCrawl/DataCrawl/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DataCrawl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetImage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetImage
+            // 
+            this.btnGetImage.Location = new System.Drawing.Point(208, 231);
+            this.btnGetImage.Name = "btnGetImage";
+            this.btnGetImage.Size = new System.Drawing.Size(75, 23);
+            this.btnGetImage.TabIndex = 0;
+            this.btnGetImage.Text = "获取图片";
+            this.btnGetImage.UseVisualStyleBackColor = true;
+            this.btnGetImage.Click += new System.EventHandler(this.btnGetImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 436);
+            this.Controls.Add(this.btnGetImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetImage;
+    }
+}
+
---
+++ Demo008/DataCrawl/DataCrawl/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace DataCrawl
+{
+    public partial class Form1 : Form
+    {
+        private static  CookieContainer cookies = new CookieContainer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetImage_Click(object sender, EventArgs e)
+        {
+            string url = "http://220.160.52.164:9085/super/pages/login/image.jsp?_time=" + DateTime.Now.ToString();
+            doGetImg(url);
+        }
+
+        private void doGetImg(string Url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url.ToString());
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "zh-CN");
+                request.Accept = "text/html, application/xhtml+xml,*/*";
+                request.KeepAlive = true;
+
+                request.UserAgent = "User-Agent Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.AllowAutoRedirect = true;
+                request.Credentials = CredentialCache.DefaultCredentials;
+                request.Host = "220.160.52.164:9085";
+                request.CookieContainer = new CookieContainer(); //暂存到新实例
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Stream responseStream = null;
+                MemoryStream ms = null;
+                if (response.ContentEncoding.ToLower() == "gzip")
+                {
+                    responseStream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress);
+                    using (var stream = responseStream)
+                    {
+                        Byte[] buffer = new Byte[4096];
+                        int offset = 0, actuallyRead = 0;
+                        do
+                        {
+                            actuallyRead = stream.Read(buffer, offset, buffer.Length - offset);
+                            offset += actuallyRead;
+                        }
+                        while (actuallyRead > 0);
+                        ms = new MemoryStream(buffer);
+                    }
+
+                }
+                cookies = request.CookieContainer; //保存cookies
+                response.Close();
+                //var cookiesstr = request.CookieContainer.GetCookieHeader(request.RequestUri); //把cookies转换成字符串
+                var img = Bitmap.FromStream(ms);
+                if (File.Exists("code.jpeg"))
+                {
+                    File.Delete("code.jpeg");
+                }
+                img.Save("code.jpg", ImageFormat.Jpeg);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
